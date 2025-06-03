026f51cbf77be2d07be21ab86b3e9e62bc129b6e
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-06-03 12:07:08 -0400

    finished single task button

--- styling-practice/src/App.tsx
+++ styling-practice/src/App.tsx
@@ -1,38 +1,16 @@
-//import { useState } from 'react'
 import './App.css'
+import { Task } from './components/Task'
 
 const exampleTask = {
   title: "Sweep the Kitchen",
   description: "Get under the cabinets, do a good job"
 }
 
-interface TaskProps {
-  taskProps: {
-    title: string;
-    description: string
-  }
-}
-
-function Task({ taskProps }: TaskProps) {
-  const title: string = taskProps.title
-  const description: string = taskProps.description
-  return(
-    <div>
-      <button className='border'/>
-      <div>
-        {title}
-        {description}
-      </div>
-    </div>
-  )
-}
-
 function App() {
-  //const [checked, setChecked] = useState(false)
 
   return (
-    <div >
-      <Task taskProps={exampleTask} />
+    <div className='flex justify-center items-center h-screen w-screen'>
+      <Task taskData={exampleTask} />
     </div>
   )
 }
