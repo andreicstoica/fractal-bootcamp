3c96497e2bb24029ebf4299ba08e0cfa5019ec0a
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-06-04 12:28:28 -0400

    setup express/vite-express server & cleaned up styling

---
+++ tic-tac-toe/server.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import ViteExpress from "vite-express";
+import { MemoryTicTacToeApi } from './src/api.ts'
+
+const app = express();
+app.use(express.json())
+
+const api = new MemoryTicTacToeApi()
+
+app.get("/message", (_, res) => res.send("Hello from express!"));
+
+app.post("/api/game", async (req, res) => {
+  const game = await api.createGame(req.body.startingPlayer)
+  res.json(game)
+})
+
+app.get("/api/game/:id", async (req, res) => {
+  const game = await api.getGame(req.params.id)
+  res.json(game)
+})
+
+app.post("/api/game/:id/move", async (req, res) => {
+  console.log('test?');
+  const game = await api.makeMove(req.params.id, req.body.coords)
+  res.json(game)
+})
+
+ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
