2abf339eea9765e46a465bc8d35f1dfaab53de28
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-06-06 11:57:09 -0400

    changed styling to be more brutalist

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -3,16 +3,19 @@ import clsx from 'clsx'
 import './App.css'
 
 function App() {
-  const centerStyle = 'flex flex-col items-center justify-center h-screen w-screen'
-  const backgroundStyle = 'bg-gradient-to-tr from-violet-100 via-slate-50 to-teal-100'
+  const centerStyle = 'flex flex-col items-center h-screen w-screen'
+  const backgroundStyle = 'bg-gradient-to-tr from-zinc-300 via-slate-100 to-gray-200'
+  const globalFontStyle = 'font-[reddit_mono]'
+  const headerStyle = 'py-5 text-5xl font-bold font-[sixtyfour] text-amber-50 bg-amber-900 shadow-md/100 shadow-amber-950 text-center self-stretch'
 
   return (
-    <div className={clsx(centerStyle, backgroundStyle)}>
-      <h1 className="text-4xl font-bold py-6">Play Tic Tac Toe!</h1>
-      <Outlet />
+    <div className={clsx(centerStyle, backgroundStyle, globalFontStyle)}>
+      <h1 className={headerStyle}>TIC TAC TOE</h1>
+      <div className="flex flex-col justify-center grow">
+        <Outlet />
+      </div>
     </div>
   )
-  //return <Game startingPlayer={startingPlayer} onRestart={setStartingPlayer} />
 }
 
 export default App
